662da551745e034771e6900fbe2550af0aec6367
jayliang701 <156070304@qq.com>
2019-03-16 12:34:19 +0800

    [WIP] improve

--- index.js
+++ index.js
@@ -2,25 +2,25 @@
  * Created by Jay Liang on 11/01/2017.
  */
 
-var args = JSON.parse(JSON.stringify(process.argv));
-var os = require('os');
+let args = JSON.parse(JSON.stringify(process.argv));
+let os = require('os');
 global.osType = os.type();
 global.isWin = osType.toLowerCase().indexOf('windows') >= 0;
 global.runArgs = args.splice(2);
-var appRootPath = require.main.filename;
+let appRootPath = require.main.filename;
 appRootPath = appRootPath.substring(0, appRootPath.lastIndexOf(global.isWin ? "\\" : "/"));
 global.appRootPath = appRootPath;
-var path = require('path');
-var params = require('yargs').argv;
+let path = require('path');
+let params = require('yargs').argv;
 if (params["version"] || params["v"]) {
-    var pkg = require(path.join(appRootPath, "package.json"));
+    let pkg = require(path.join(appRootPath, "package.json"));
     console.log(pkg.version);
     return process.exit();
 }
 
-var cmd = global.runArgs.shift();
+let cmd = global.runArgs.shift();
 
-var script = require("./lib/" + cmd);
+let script = require("./lib/" + cmd);
 script.exec(global.runArgs, function() {
     process.exit();
 });
